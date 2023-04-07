21830adb05c415df2c5f15b917079daadd30fb45
Chris Kaczor <chris@kaczorzoo.net>
2023-04-07 11:14:41 -0400

    Initial commit

---
+++ ApplicationExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Reflection;
+using Microsoft.Win32;
+
+namespace ChrisKaczor.Wpf.Application
+{
+    public static class ApplicationExtensions
+    {
+        public static void SetStartWithWindows(this System.Windows.Application application, bool value)
+        {
+            var applicationName = Assembly.GetEntryAssembly()!.GetName().Name;
+
+            SetStartWithWindows(application, applicationName!, value);
+        }
+
+        public static void SetStartWithWindows(this System.Windows.Application application, string applicationName, bool value)
+        {
+            var applicationPath = $"\"{Assembly.GetEntryAssembly()!.Location}\"";
+
+            SetStartWithWindows(application, applicationName, applicationPath, value);
+        }
+
+        public static void SetStartWithWindows(this System.Windows.Application _, string applicationName, string applicationPath, bool value)
+        {
+            // Open the run registry key
+            var registryKey = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run", true) ?? throw new ApplicationException("Unable to open registry key");
+
+            // Delete any existing key
+            registryKey.DeleteValue(applicationName, false);
+
+            // If auto start should not be on then we're done
+            if (!value) return;
+
+            // Set the registry key
+            registryKey.SetValue(applicationName, applicationPath);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+﻿# ChrisKaczor.Wpf.Application.StartWithWindows
+
+This library can be used to set an application to auto-start when Windows starts.
+
+## Authors
+
+* **Chris Kaczor** - *Initial work* - https://github.com/ckaczor - https://chriskaczor.com
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details.
+
+## Disclaimer
+
+This code is used by my various personal projects and may not be useful or work in all use cases.
